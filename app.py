9fa8a0dda8189551e5d0a9ffca28d227f539a057
Sourav B. Roy <wildr.slimshady@gmail.com>
2024-10-14 17:16:47 +0530

    - add a new route to test github actions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from data.model_enums import Catagory
 from data.pydantic_models import Message
 
 app = FastAPI()
@@ -10,4 +11,11 @@ async def root():
 
 @app.post('/contact')
 async def contact(message: Message):
+    return {'message' : message}
+
+
+@app.post('/test')
+async def contact(message: Message=None):
+    if not message:
+        message = Message(name='sourav', message='test github actions', datetime=datetime.now(), email='test@test.com', catagory=Catagory.JOB.value)
     return {'message' : message}
